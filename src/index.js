130e3e0c3db668b74b52bb9a3f391fa1d750bb8b
jonathan.cembranos.casa <jonathan.cembranos.trabajo@gmail.com>
2021-01-03 17:21:17 -0300

    actualisasion

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import Exercises from './pages/Exercises'
 //import Films from './components/films'
 //import Login from './components/login'
 //import App from './App'
-import Card from './components/card'
 
-const container =document.getElementById('root')
 
-ReactDOM.render(<Card />,container)
+const container = document.getElementById('root')
+
+ReactDOM.render(<Exercises />, container)
